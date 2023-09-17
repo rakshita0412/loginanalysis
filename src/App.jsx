f561e34cf99691289595f32d10e97dfc8eebdbab
rakshita0412 <rakshitavipperla@gmail.com>
2023-09-17 17:10:47 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,50 @@ import Signup from './components/signup'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Dashboard from './components/Dashboard'
 import Homepage from './components/Homepage'
+import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useEffect,useState } from 'react'
 
 function App() {
-  
+  const[allusers, setAllUsers] = useState() 
+  useEffect(() => {
+    axios.get('https://localhost:3001/getUsers')
+    .then((data) => {console.warn(data);setAllUsers(data)})
+    .catch(err=> console.log(err))
+  }, [])
+
+  return(
+    <div className = "w-100 vh-100 d-flex justify-content-center align-items-center">
+      <div className="w-50"> 
+      <table className="table">
+        <thead>
+          <tr>
+            <th>
+              Name
+            </th>
+            <th>
+              Email
+            </th>
+            <th>
+              Password
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {
+            allusers.map(user => {
+              <tr>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.password}</td>
+              </tr>
+            })
+          } */}
+        </tbody>
+      </table>
+      </div>
+    </div>
+  )
   return (
     <BrowserRouter>
       <Routes>
